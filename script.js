d9c366a3753e8dd3cec4f09ee3570a4e82f6505e
Anne McCorkle <annefmccorkle@gmail.com>
2022-04-04 10:47:41 -0600

    created some js, added the carousel to it, the apistuff is youtube notes

--- script.js
+++ script.js
@@ -1,73 +1,15 @@
-// var musicApiKey = 
-var youTubeApiKey ="AIzaSyDtZK7v4AhM4wyZDLwKYDslq41EM0i3Gzw"
-
-$(document).ready(function () {
-  //button click function
-
-
-//the way I typed this, the curly brackets will be a searched variable, our key is in there, and it allows for 25 results to appear on a page; "kind":  "id": "youtube#video"
-  var requestUrl = "http://www.googleapis.com/youtube/v3/search?part=snippet&q={the weeknd}&key=AIzaSyDtZK7v4AhM4wyZDLwKYDslq41EM0i3Gzw&maxResults=25";
-  fetch(requestUrl) 
-    .then(
-      function (response) {
-        return response.json();
-      })
-    .then(function (data) {
-      console.log(data);
-      return data;
-    })
-  
-})
-
-$(document).ready(function () {
-  $('.carousel').carousel();
-});
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.carousel');
-//     var instances = M.Carousel.init(elems, options);
-//   });
 
-//   // Or with jQuery
-
-//   $(document).ready(function(){
-//     $('.carousel').carousel();
-//   });
-
-
-// function allowDrop(ev) {
-//   ev.preventDefault();
-// }
-
-// function drag(ev) {
-//   ev.dataTransfer.setData("text", ev.target.id);
-// }
-
-// function drop(ev) {
-//   ev.preventDefault();
-//   var data = ev.dataTransfer.getData("text");
-//   ev.target.appendChild(document.getElementById(data));
-// }
-
-
-// var instance = M.Carousel.init({
-//     fullWidth: true,
-//     indicators: true
-//   });
-$(document).ready(function(){
-    $('#demo-carousel').carousel({
-        numVisible: 3
-    });
-
-  });
+// $(document).ready(function () {
+//   $('.carousel').carousel();
+// });
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.carousel');
-//     var instances = M.Carousel.init(elems);
-//   });
 
-//Youtube API
+
+
+
+
--- youtube.js
+++ youtube.js
@@ -1,56 +1,50 @@
-var channelInput = $("#channel-input");
-var savedVideoCard = $("#video-container");
-var searchBtn = $("#search-button")
+var channelInput = document.getElementById("channel-input");
+var savedVideoCardEl = document.getElementById("video-container");
+var searchBtn = document.getElementById("search-button");
+// if we need a client id add here
+// discovery doc, scopes, authorizeButton
+var contentEl = document.getElementById("content");
+var channelFormEl = document.getElementById("channel-form");
+var channelInput = document.getElementById("channel-input");
+
+var videoContainer = document.getElementById("video-container");
 
 
-$(document).ready(function() {
 
-  //not loading???
-  // $(document).ready(function () {
-  //   $('.carousel').carousel();
-  // });
+//this connects to the array and saves any artist looked at right now and allow us to pull that artist back up
+var savedArray = JSON.parse(window.localStorage.getItem("savedArray")) || [];
+
 
   
+  $(document).ready(function () {
+    $('.carousel').carousel();
+  });
 
   //gathers the info if users search
-  $(searchBtn).on("click", function() {
-    var userSearch = $(channelInput).val(); 
-    $("search-value").val("").trim();
-    searchForVideo(userSearch);
+  searchBtn.on("click", function() {
+    var userSearch = channelInput.value(); 
+    channelInput.value("").trim();
+    searchByKeyword(userSearch);
   })  
   
-  //takes the artist card and saves it
+  //takes the artist card and saves it, (drag and drop function)?
   function makeSavedGroup(text) {
-    var li = $("<li>").addClass("list-group-item").text(text);
-    savedVideoCard.append(li);
+    var li = document.createElement("<li>").addClass("list-group-item").textContent(text);
+    savedVideoCardEl.appendChild(li);
   }
   
   
-  //this grabs saved the videos to watch again
-  savedVideoCard.on("click", "li", function() {
-    searchForVideo($(this).text());
+  //this grabs saved the videos to watch again later, 
+  savedVideoCardEl.on("click", "li", function() {
+    searchByKeyword($(this).textConent());
+ 
+    if (savedArray.length < 0) {
+      searchForVideo(savedArray[savedArray.length -1]);
+    }
+
+    for (var i = 0; i < savedArray.length; i++) {
+      makeSavedGroup(searchForVideo[i]);
+    }
   })
-
-
-
   
-  function searchForVideo(userSearch) {
-    var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-    for(var i in results.items) {
-          var item = results.items[i];
-          Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-        }
-      }
 
-  //this saves any artist looked at right now and allow us to pull that artist back up
-  var savedArray = JSON.parse(window.localStorage.getItem("savedArray")) || [];
-  
-  if (savedArray.length < 0) {
-    searchForVideo(savedArray[savedArray.length -1]);
-  }
-
-  for (var i = 0; i < savedArray.length; i++) {
-    makeSavedGroup(searchForVideo[i]);
-  }
-
-})
