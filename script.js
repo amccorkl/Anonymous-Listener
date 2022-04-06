540262e3671e6e9ffac2fca16d52b05ce6444f4f
Anne McCorkle <annefmccorkle@gmail.com>
2022-04-06 13:09:10 -0600

    trying to search for a video with youtube api

--- script.js
+++ script.js
@@ -13,25 +13,6 @@ $(document).ready(function () {
  //right now the html will only work with those 5 videos - need a js carousel that inserts to an empty html div
   $('.carousel').carousel();
 
-
-//have the music albums load upon page loading, have everything embedded in the document.ready function
-
-  //function openVideo () {
-  //connect with the youtube video aLink; // We need a function with a js "this.value.videoId" <a> tag/onclick function; not sure this is the right syntax for clicking on a video:
-  //videoSelected = target.this.value.videoId();
-
-
-
-
-$(document).ready(function () {
-  //need an array that saves any artist looked at right now and allow us to pull that artist back up
-
-
-  //access the music api for play list items to show on UI
-  //right now the html will only work with those 5 videos - need a js carousel that inserts to an empty html div
-  $('.carousel').carousel();
-
-
   //function openVideo () {
   //connect with the youtube video aLink; // We need a function with a js "this.value.videoId" <a> tag/onclick function; not sure this is the right syntax for clicking on a video:
   //videoSelected = target.this.value.videoId();
@@ -115,5 +96,7 @@ fetch(requestUrl, { headers: { apikey: NapsterAPIKey } })
     console.log(data);
   })
 
-})
+var songTitleArray = ["Loves Me Like a Rock", "Jagged Little Pill"];
+localStorage.setItem("songTitleArray", JSON.stringify(songTitleArray));
+
 
