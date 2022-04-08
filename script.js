5bd3542cd573a83ac7dd2dfb41e9875019a8dc4c
Anne McCorkle <annefmccorkle@gmail.com>
2022-04-07 19:00:01 -0600

    cleaning up the html

--- script.js
+++ script.js
@@ -60,29 +60,9 @@ var words = [
 
 //have the music albums load upon page loading, have everything embedded in the document.ready function
 $(document).ready(function () {
-  //need an array that saves any artist looked at right now and allow us to pull that artist back up
-
-
+  
  //access the music api for play list items to show on UI
- //right now the html will only work with those 5 videos - need a js carousel that inserts to an empty html div
-  $('.carousel').carousel();
-
-  //function openVideo () {
-  //connect with the youtube video aLink; // We need a function with a js "this.value.videoId" <a> tag/onclick function; not sure this is the right syntax for clicking on a video:
-  //videoSelected = target.this.value.videoId();
-
-
-  //takes the artist video and saves it, (drag and drop function)
-  // save it to local storage as well, then we don't need to reaccess youtube
-  //function makeSavedGroup()
-
-  //need a function that clicks on the saved music image to replay video
-});
-
-
-
-
-
+   $('.carousel').carousel();
 
 
 
@@ -154,7 +134,7 @@ fetch(requestUrl, { headers: { apikey: NapsterAPIKey } })
 
 var songTitleArray = ["Loves Me Like a Rock", "Jagged Little Pill"];
 localStorage.setItem("songTitleArray", JSON.stringify(songTitleArray));
-})
+
 
 
 // Function that grabs the data from the images array // 
@@ -178,3 +158,4 @@ for (var i = 0; i < data.length; i++) {
 }
 
 
+})
